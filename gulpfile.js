3835120cc775944d99105eff0b80261b41a658a2
Prayash Thapa <prayasht@gmail.com>
2016-05-12 00:22:34 -0600

    Gettin' that build process!

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,40 @@
 var gulp = require('gulp');
+var gulpIf = require('gulp-if');
 var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var minifyCss = require('gulp-minify-css');
+var browserSync = require('browser-sync').create();
 
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
 
-gulp.task('sass', function(done) {
-  gulp.src('src/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('css/'))
-    .on('end', done);
-});
-
-gulp.task('styles', function() {
+gulp.task('sass', function() {
   gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css/'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('src/css/'))
+    .pipe(browserSync.reload({ stream: true }))
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.scss', ['styles']);
+gulp.task('watch', ['browserSync', 'sass', 'dist'], function() {
+  gulp.watch('src/scss/**/*.scss', ['sass', 'dist']);
+  gulp.watch('src/*.html', ['dist']);
+  gulp.watch('src/*.js', ['dist']);
+});
+
+gulp.task('dist', function() {
+  return gulp.src('src/index.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', minifyCss()))
+    .pipe(gulp.dest(''))
+    .pipe(browserSync.reload({ stream: true }))
 });
