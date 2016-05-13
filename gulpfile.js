d6a769fabf06d35c57c98d717456dd5c9e3e1ea9
Prayash Thapa <prayasht@gmail.com>
2016-05-12 23:48:20 -0600

    Gulp cleanage.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+// gulpfile.js
+// ************************************************************************************
+
 var gulp = require('gulp');
 var gulpIf = require('gulp-if');
 var sass = require('gulp-sass');
@@ -8,14 +11,14 @@ var cssnano = require('gulp-cssnano');
 var minifyCss = require('gulp-minify-css');
 var browserSync = require('browser-sync').create();
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: ''
-    },
-  })
-})
+// ************************************************************************************
 
+// - Live Reload
+gulp.task('browserSync', function() {
+  browserSync.init({ server: { baseDir: '' } })
+});
+
+// - Sass Transpiler
 gulp.task('sass', function() {
   gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -24,15 +27,18 @@ gulp.task('sass', function() {
     .pipe(browserSync.reload({ stream: true }))
 });
 
+// - Watcher & Server
 gulp.task('serve', ['browserSync', 'sass', 'dist'], function() {
   gulp.watch('src/scss/**/*.scss', ['sass', 'dist']);
   gulp.watch('src/*.html', ['dist']);
   gulp.watch('src/js/*.js', ['dist']);
 });
 
+// - Distribution
 gulp.task('dist', function() {
   return gulp.src('src/index.html')
     .pipe(useref())
+    // Uncomment these for full uglification
     // .pipe(gulpIf('*.js', uglify()))
     // .pipe(gulpIf('*.css', minifyCss()))
     .pipe(gulp.dest(''))
