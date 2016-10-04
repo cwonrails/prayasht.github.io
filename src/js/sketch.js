fe97af96eafaced2e15ed5228071c98baa342ab4
Prayash Thapa <prayasht@gmail.com>
2016-10-03 19:49:47 -0600

    Modify p5 sketch.

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,4 +1,6 @@
-// Fragments
+// Prayash Thapa (effulgence.io)
+// Portfolio Fragments
+
 var num = 50, frames = 300, edge = 40;
 var fragments = [];
 var theta = 0;
@@ -9,11 +11,11 @@ function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("content"); frameRate(30);
   background('#EBEBEB');
-  
+
   if (windowWidth >= 780) num = 75;
   else num = 25;
 
-  // Fragments
+  // * Fragments
   for (var i = 0; i < num; i++) {
     var x = random(windowWidth);
     var y = (windowHeight - 2) / (num) * i;
@@ -27,13 +29,8 @@ function setup() {
 
 function draw() {
   background('#EBEBEB');
+  colorMode(RGB); strokeWeight(2);
 
-  // ---------------
-  // Fragments
-  colorMode(RGB);
-  noFill();
-  stroke(0, 25);
-  strokeWeight(2);
   for (var i = 0; i < fragments.length; i++) fragments[i].render();
   theta += TWO_PI/frames * (0.1);
 }
@@ -42,8 +39,7 @@ function draw() {
 function Fragment(_x, _y) {
   var x, y;
   var px, py, offSet, radius;
-  var dir;
-  var col; var currentOrb;
+  var dir, col, currentOrb, opacity;
 
   this.x = _x;
   this.y = _y;
@@ -63,14 +59,28 @@ function Fragment(_x, _y) {
   }
 
   this.display = function() {
+    ellipse(px, py, 4, 4);
     for (var i = 0; i < fragments.length; i++) {
-      var distance = dist(px, py, fragments[i].px, fragments[i].py);
+      // Calculate distance from current node to every other
+      var p2 = fragments[i];
+      var distance = dist(px, py, p2.px, p2.py);
 
+      // Only draw if certain distance between nodes
       if (distance > 30 && distance < 125) {
-        strokeCap(ROUND);
+        strokeCap(ROUND); stroke(0, 25);
         line(px, py, fragments[i].px, fragments[i].py);
-        // ellipse(px, py, 5, 5);
-        // ellipse(fragments[i].px, fragments[i].py, 10, 10);
+      }
+
+      // Inner loop further enumerates to all other vertices to calculate triangulation
+      for (var j = i + 1; j < fragments.length; j++) {
+        var p3 = fragments[j];
+        var dist2 = dist(p2.px, p2.py, p3.px, p3.py);
+        var dist3 = dist(px, py, p3.px, p3.py)
+        // Triangulation
+        if (distance <= 120 && dist2 <= 120 && dist3 <= 120) {
+          noStroke(); fill(32, 32, 32, 10);
+          // triangle(px, py, p2.px, p2.py, p3.px, p3.py);
+        }
       }
     }
   }
