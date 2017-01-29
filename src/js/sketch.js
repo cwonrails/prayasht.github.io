d3bd9042c86dd947320c707c70f3138c64c814e2
Prayash Thapa <prayasht@gmail.com>
2017-01-29 13:21:23 -0700

    mesh terrain

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,27 +1,30 @@
 // Prayash Thapa (effulgence.io)
 // Portfolio Fragments
 
-var num = 50, frames = 300, edge = 40;
-var fragments = [];
-var theta = 0;
+var cols, rows;
+var scl = 25;
+var w = 1980;
+var h = 800;
+var fade = 255;
+var flying = 0;
+
+var terrain;
 
 // ************************************************************************************
 
 function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("content"); frameRate(30);
+  var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas.parent("content");
   background('#EBEBEB');
+  frameRate(30);
+  colorMode(RGB);
 
-  if (windowWidth >= 780) num = 75;
-  else num = 25;
+  cols = w / scl;
+  rows = h / scl;
 
-  // * Fragments
-  for (var i = 0; i < num; i++) {
-    var x = random(windowWidth);
-    var y = (windowHeight - 2) / (num) * i;
-    fragments.push(new Fragment(x, y));
-    fragments[i].px = random(windowWidth);
-    fragments[i].py = random(windowHeight);
+  terrain = [];
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
   }
 }
 
@@ -29,60 +32,41 @@ function setup() {
 
 function draw() {
   background('#EBEBEB');
-  colorMode(RGB); strokeWeight(2);
+  strokeWeight(2);
+  stroke(228, 238, 238);
 
-  for (var i = 0; i < fragments.length; i++) fragments[i].render();
-  theta += TWO_PI/frames * (0.1);
-}
-
-// Fragment
-function Fragment(_x, _y) {
-  var x, y;
-  var px, py, offSet, radius;
-  var dir, col, currentOrb, opacity;
-
-  this.x = _x;
-  this.y = _y;
-  offSet = random(TWO_PI);
-  radius = random(5, 10);
-  dir = random(1) > .5 ? 1 : -1;
-
-  this.render = function() {
-    this.update();
-    this.display();
-  };
-
-  this.update = function() {
-    var vari = map(sin(theta + offSet), -1, 1, -2, -2);
-    px = map(sin(theta + offSet) , -1, 1, 0, width);
-    py = this.y + sin(theta * dir) * radius * vari;
+  flying -= 0.01;
+  var yOff = flying;
+  for (var y = 0; y < rows; y++) {
+    var xOff = 0;
+    for (var x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xOff, yOff), 0, 1, -200, 200);
+      xOff += 0.1;
+    }
+    yOff += 0.05;
   }
 
-  this.display = function() {
-    ellipse(px, py, 4, 4);
-    for (var i = 0; i < fragments.length; i++) {
-      // Calculate distance from current node to every other
-      var p2 = fragments[i];
-      var distance = dist(px, py, p2.px, p2.py);
+  rotateX(-PI/2);
+  translate(-w/2, -h/4);
 
-      // Only draw if certain distance between nodes
-      if (distance > 30 && distance < 125) {
-        strokeCap(ROUND); stroke(0, 25);
-        line(px, py, p2.px, p2.py);
-      }
+  var locY = (mouseY / height - 0.5) * (2);
+  var locX = (mouseX / width - 0.5) * 2;
 
-      // Inner loop further enumerates to all other vertices to calculate triangulation
-      // for (var j = i + 1; j < fragments.length; j++) {
-      //   var p3 = fragments[j];
-      //   var dist2 = dist(p2.px, p2.py, p3.px, p3.py);
-      //   var dist3 = dist(px, py, p3.px, p3.py)
-      //   // Triangulation
-      //   if (distance <= 120 && dist2 <= 120 && dist3 <= 120) {
-      //     noStroke(); fill(32, 32, 32, 10);
-      //     triangle(px, py, p2.px, p2.py, p3.px, p3.py);
-      //   }
-      // }
+  var hue = map(frameCount, 0, 255, 0, 255);
+  ambientLight(10, 90, hue);
+  pointLight(250, 250, 250, locX, locY, 255);
+  ambientMaterial(155, 155);
+
+  for (var y = 0; y < rows - 1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    stroke(0);
+    for (var x = 0; x < cols; x++) {
+      push();
+      vertex(x * scl, y * scl, terrain[x][y]);
+      vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
+      pop();
     }
+    endShape();
   }
 }
 
